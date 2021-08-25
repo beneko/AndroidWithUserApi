b60ba12d2b5c080af382a9dd07a54785492a804d
beneko <behzadnekouei@gmail.com>
2021-08-25 16:55:24 +0200

    update project

---
+++ app/src/main/java/com/afpa/androidwithuserapi/Constants.java
@@ -0,0 +1,9 @@
+package com.afpa.androidwithuserapi;
+
+public class Constants {
+
+    // la base url de note api
+    private static final String ROOT_URL = "https://10.0.2.2:8080/v1/";
+    // le nom du fichier utilisé
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+}
--- app/src/main/java/com/afpa/androidwithuserapi/MainActivity.java
+++ app/src/main/java/com/afpa/androidwithuserapi/MainActivity.java
@@ -1,14 +1,101 @@
 package com.afpa.androidwithuserapi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    // les champs
+    private EditText editTextUserName,editTextEmail, editTextPassword;
+    // le bouton
+    private Button buttonRegister;
+    // le progressdialog
+    private ProgressDialog progressDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextUserName = findViewById(R.id.editTextUserName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(this::onClick);
+
+    }
+
+    public void onClick(View v) {
+        // on se donne la possibilité d'avoir plusieurs actionneurs
+        if (v == buttonRegister)
+            registerUser();
+    }
+
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUserName.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        // On affiche un popup de progression
+//        progressDialog.setMessage("Enregistrement utilisateur");
+//        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_REGISTER, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // arret du popup
+//                progressDialog.dismiss();
+                // récupération du Json de réponse
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                // affichage de la réponse
+                    Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                progressDialog.hide();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue =  Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
