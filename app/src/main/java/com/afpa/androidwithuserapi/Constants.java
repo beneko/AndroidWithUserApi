d2ea369ecefe7f579c9dbc17833711f4bf74b7be
beneko <behzadnekouei@gmail.com>
2021-09-02 16:39:42 +0200

    internet permission and localhost url updated and application tested and it is working

--- app/src/main/java/com/afpa/androidwithuserapi/Constants.java
+++ app/src/main/java/com/afpa/androidwithuserapi/Constants.java
@@ -3,7 +3,7 @@ package com.afpa.androidwithuserapi;
 public class Constants {
 
     // la base url de note api
-    private static final String ROOT_URL = "https://10.0.2.2:8080/v1/";
+    private static final String ROOT_URL = "http://10.0.2.2:8000/v1/";
     // le nom du fichier utilisé
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
 }
